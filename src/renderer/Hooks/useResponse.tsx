be79ce3fa3699e9a9b3aae5ecb79759aabd6007b
karna98 <vedantwakalkar@gmail.com>
2022-01-10 00:52:00 +0530

    Restructured Auth.

---
+++ src/renderer/Hooks/useResponse.tsx
@@ -0,0 +1,86 @@
+/**
+ * useResponse.tsx
+ *
+ * Description:
+ *    Response Hook to reolve received response .
+ *
+ */
+
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { useNavigate } from 'react-router-dom';
+import { Dispatch } from 'redux';
+import { reactRoutes } from '../../common/routes';
+import { IPCRequestObject } from '../../common/util';
+import { updateMessageState, updateSessionState } from '../State/reducer';
+import { sendToIpcMain } from '../util';
+
+/**
+ * Displays Message.
+ *
+ * @param status Message Status.
+ * @param message Message.
+ */
+const dispatchMessage = (
+  dispatch: Dispatch,
+  status: number,
+  message: string | undefined
+) => {
+  // Update Message in Redux Store..
+  message && dispatch(updateMessageState(status, message));
+};
+
+const useResponse = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const resolveResponse = (responseData: IPCResponseInterface) => {
+    switch (responseData.URI) {
+      case 'auth-status':
+        // Based on auth-status, Login or Register Page will be displayed.
+        responseData.status == 200 && responseData.data == 0
+          ? // Register Page
+            navigate(reactRoutes.auth_register)
+          : // Login Page
+            navigate(reactRoutes.auth_login);
+        break;
+
+      case 'auth-register':
+        // Display response message.
+        dispatchMessage(
+          dispatch,
+          responseData.status as number,
+          responseData.message
+        );
+
+        // Set Registration status to true.
+        responseData.status == 200 && navigate(reactRoutes.auth_login);
+        break;
+
+      case 'auth-login':
+        // Set Message to be displayed.
+        dispatchMessage(
+          dispatch,
+          responseData.status as number,
+          responseData.message
+        );
+
+        if (responseData.status == 200) {
+          dispatch(updateSessionState(responseData.data as SessionType));
+          sendToIpcMain(IPCRequestObject(`spaces-get`));
+
+          // Redirect to Home page.
+          navigate(reactRoutes.spaces);
+        }
+        break;
+    }
+  };
+
+  useEffect(() => {
+    window.NotesAPI.receive(`fromMain`, (responseData: string) => {
+      resolveResponse(JSON.parse(responseData));
+    });
+  }, []);
+};
+
+export default useResponse;
